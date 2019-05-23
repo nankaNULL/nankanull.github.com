8cdaabb6e1ba7e44bd69b0208b1c136a616c13c9
nankaNULL <534129087@qq.com>
2019-05-23 14:37:57 +0800

    切换看板娘

--- src/components/live2d/index.js
+++ src/components/live2d/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { loadFiles } from './util';
+import _ from 'lodash';
+import classNames from 'classnames';
+import './style.scss';
 
 export default class live2d extends React.PureComponent{
   state = {
@@ -11,7 +14,8 @@ export default class live2d extends React.PureComponent{
       width: '500',
       message: '我是 model-2 点击能触发一个音效',
       offset: '93%',
-    }
+    },
+    modalId: 2
   }
 
   componentDidMount () {
@@ -23,24 +27,51 @@ export default class live2d extends React.PureComponent{
     const { currModel } = this.state;
     loadFiles('./constants/live2d/js/live2d.js')
     .then(() => {
-      let loadlive2d = window.loadlive2d;
-      loadlive2d("live2d", `./constants/live2d/${currModel.name}/model.json`)
+      this.setState({loadlive2d: window.loadlive2d}, () => {
+        loadlive2d("live2d", `./constants/live2d/${currModel.name}/model.json`)
+      })
     })
+  }
+
+  modalChange = ( modalId ) => {
+    this.setState({ 
+      modalId,
+      currModel: Object.assign({}, this.state.currModel, {
+        name: `model-${modalId}`,
+        message: `我是 model-${modalId} 点击能触发一个音效`
+      })
+    }, () => {
+      window.loadlive2d("live2d", `./constants/live2d/${this.state.currModel.name}/model.json`)
+    });
   }
   
   render () {
-    const { currModel } = this.state;
+    const { currModel, modalId } = this.state;
     const boxStyle = {
       width: `${currModel.width}px`, 
       height: `${currModel.height}px`,
     }
+    const modals = [1, 2, 3, 4, 5, 6];
     return (
-      <div style={boxStyle}>
+      <div className="page-component-live2d" style={boxStyle}>
         <canvas 
           id="live2d" 
           width={currModel.width} 
           height={currModel.height}>
         </canvas>
+        <div className="modal-tab">
+          {
+            modals.map((item, index) => (
+              <div key={index} 
+                className={classNames("tab-item pointer", { active: item == modalId })}
+                onClick={this.modalChange.bind(this, item)}>
+                modal {item}
+              </div>
+              
+            ))
+          }
+          <div></div>
+        </div>
       </div>
     )
   }
