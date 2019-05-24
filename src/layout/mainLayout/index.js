26bddfd63f64f1954b3dfafd81060ceb70e6d44d
nankaNULL <534129087@qq.com>
2019-05-24 11:57:26 +0800

    自适应

--- src/layout/mainLayout/index.js
+++ src/layout/mainLayout/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Popover } from 'antd';
 import { Link } from 'react-router-dom';
 import './style.scss';
 const { Header, Content, Footer } = Layout;
@@ -28,12 +28,24 @@ export default class MainLayout extends React.PureComponent{
         <Header className="header">
           <div className="header-logo">LOGO</div>
           <Menu 
+            className="menu-info"
             mode="horizontal" 
             selectedKeys={topNavData.filter((topNav) => pathname.indexOf(topNav.url)>-1).map((topNav) => topNav.url)}>
             {topNavData.map((item) => (
               <Menu.Item className="top-nav-item" key={item.url}><Link to={item.url}>{item.title}</Link></Menu.Item>
             ))}
           </Menu>
+         
+          <Popover 
+            content={topNavData.map((item) => ( <div key={item.url}><Link to={item.url}>{item.title}</Link></div>))} 
+            placement="bottom"
+            >
+            <div className="menu-tab">
+              <div className="menu-tab-item"></div>
+              <div className="menu-tab-item"></div>
+              <div className="menu-tab-item"></div>
+            </div>
+          </Popover>
         </Header>
         <Layout className="content">
           {this.props.children}
